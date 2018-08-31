b972ec0130ae3a0ae9bfb889f5f61e36a3cdb475
EVA VICTORIA DA SILVA SOUZA <evavic.ifpe@gmail.com>
2018-08-31 15:39:23 -0300

    Exercicio 2 resolvido

--- src/br/edu/ifpe/java8/ex2/RelatorioAlunos2.java
+++ src/br/edu/ifpe/java8/ex2/RelatorioAlunos2.java
@@ -3,8 +3,10 @@ package br.edu.ifpe.java8.ex2;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-
+import java.util.function.Function;
 import br.edu.ifpe.java8.entidades.Aluno;
 
 /**
@@ -30,6 +32,16 @@ public class RelatorioAlunos2 {
 
 	private static final String SEPARADOR = "#";
 
+	private static final Map<TipoRelatorio2, Function<Aluno, String>> OPERACOES;
+	static {
+		OPERACOES = new HashMap<>();
+		OPERACOES.put(TipoRelatorio2.NOME, aluno->aluno.getNome());
+		OPERACOES.put(TipoRelatorio2.NOME_MATRICULA, aluno->aluno.getNome() + SEPARADOR + aluno.getMatricula());
+		OPERACOES.put(TipoRelatorio2.NOME_MATRICULA_MEDIA, aluno->aluno.getNome() + SEPARADOR + aluno.getMatricula() + SEPARADOR + aluno.getMedia());
+		OPERACOES.put(TipoRelatorio2.NOME_MEDIA, aluno->aluno.getNome() + SEPARADOR + aluno.getMedia());
+		OPERACOES.put(TipoRelatorio2.MATRICULA_MEDIA, aluno->aluno.getMatricula() + SEPARADOR + aluno.getMedia());
+	}
+
 	public static void gerar(TipoRelatorio2 opcao) {
 		File arquivoEntrada = new File("alunos.txt");
 		File arquivoSaida = new File("relatorio.txt");
@@ -51,23 +63,6 @@ public class RelatorioAlunos2 {
 				aluno.setMatricula(valores[1]);
 				aluno.setMedia(Double.parseDouble(valores[2]));
 
-				switch (opcao) {
-				case NOME:
-					escritor.println(aluno.getNome());
-					break;
-				case NOME_MATRICULA:
-					escritor.println(aluno.getNome() + SEPARADOR + aluno.getMatricula());
-					break;
-				case NOME_MATRICULA_MEDIA:
-					escritor.println(aluno.getNome() + SEPARADOR + aluno.getMatricula() + SEPARADOR + aluno.getMedia());
-					break;
-				case NOME_MEDIA:
-					escritor.println(aluno.getNome() + SEPARADOR + aluno.getMedia());
-					break;
-				case MATRICULA_MEDIA:
-					escritor.println(aluno.getMatricula() + SEPARADOR + aluno.getMedia());
-					break;
-				}
 			}
 		} catch (IOException e) {
 			/* DO NOTHING */
